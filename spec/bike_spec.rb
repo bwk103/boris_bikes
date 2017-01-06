f38b176d2f01cec9882ddfac59d1537e8bb88e8e
Shoniwa Marovatsanga <stmarovatsanga@gmail.com>
2017-01-06 15:57:32 +0000

    testing if report broken returns broken

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -10,5 +10,9 @@ describe "when bike is broken" do
   it "allows the user to report that the bike is broken" do
     expect(subject).to respond_to(:report_broken)
   end
+  it "sets broken to true" do
+    bike.report_broken
+    expect(bike.report_broken).to eq true
+  end
 end
 end
